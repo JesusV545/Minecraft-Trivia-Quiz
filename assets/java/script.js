567165d3bdd7e4c10444958b3f540dc2cf843740
Jesus <117941643+JesusV545@users.noreply.github.com>
2023-01-11 17:08:57 -0600

    attempted to fix multiple bugs

--- assets/java/script.js
+++ assets/java/script.js
@@ -5,25 +5,23 @@ let finalScores = document.querySelector('#final-score');
 let gameOver = document.querySelector('.game-over');
 let gameQuestions = document.querySelector('#questions');
 let timer = document.querySelector('#timer');
-let startQuizBtn = document.querySelector('#big-btn'); 
-let startQuizSection = document.querySelector('.main-body');
+let startQuizBtn = document.querySelector('.big-btn'); 
+let startQuizSection = document.querySelector('.start-page');
 let scoreSectionDiv = document.querySelector('.highscore-container');
 let scoreSection = document.querySelector('#highscore-page');
-let inputName = document.querySelector('#score-submission');
+let inputName = document.querySelector('#input-name');
 let displayName = document.querySelector('#user-names');
 let endGameBtn =document.querySelector('.end-game-btns');
 let submitBtn = document.querySelector('#score-submission');
 let scoreDisplay = document.querySelector('#highscore-score');
+
+let clearBtn = document.querySelector("#clear-scores");
+let playAgainBtn = document.querySelector("#play-again-btn");
+
 let btnA =document.querySelector('#A');
 let btnB = document.querySelector('#B');
 let btnC = document.querySelector('#C');
 let btnD =document.querySelector('#D');
-let finalQuestionIndex = quizQuestions.length;
-let currentQuestionIndex = 0;
-let timeLeft = 100;
-let timerInterval;
-let score = 0;
-let correct;
 let quizQuestions = [
     {
         question: "What's is the most durable ore in the game?",
@@ -31,63 +29,69 @@ let quizQuestions = [
         choiceB: "Gold",
         choiceC: "Iron",
         choiceD: "Netherite",
-        correctAnswer: "Netherite"},
+        correctAnswer: "D"},
     {
         question: "Which mob does the most damage to the player?",
         choiceA: "Warden",
         choiceB: "Piglin Brute",
         choiceC: "Charged Creeper",
         choiceD: "Vindicator",
-        correctAnswer: "Charged Creeper"},
+        correctAnswer: "C"},
     {
         question: "What ore do you need to craft a bucket?",
         choiceA: "Copper",
         choiceB: "Iron",
         choiceC: "Gold",
         choiceD: "Diamond",
-        correctAnswer: "Iron"},
+        correctAnswer: "B"},
     {
         question: "What mob(s) do villagers create when they feel in danger?",
         choiceA: "Iron Golem",
         choiceB: "Ghast",
         choiceC: "Wither",
         choiceD: "Dog",
-        correctAnswer: "Iron Golem"},
+        correctAnswer: "A"},
     {
         question: "What food do you use to tame a cat?",
         choiceA: "Steak",
         choiceB: "Potatos",
         choiceC: "Carrots",
         choiceD: "Fish",
-        correctAnswer: "Fish"},
+        correctAnswer: "D"},
     {
         question: "What mob has the most health?",
         choiceA: "Ender Dragon",
         choiceB: "Iron Golem",
         choiceC: "Warden",
         choiceD: "Wither",
-        correctAnswer: "Warden"},
+        correctAnswer: "C"},
     {
         question: "How many bookshelves do you need to get a full level 30 enchantment?",
         choiceA: "18",
         choiceB: "10",
         choiceC: "15",
         choiceD: "12",
-        correctAnswer: "15"},
+        correctAnswer: "C"},
     {
         question: "In what biome can you find a village?",
         choiceA: "Grassland",
         choiceB: "Savannah",
         choiceC: "Taiga",
         choiceD: "All of the above",
-        correctAnswer: "All of the above"},
+        correctAnswer: "D"},
 ];
+let finalQuestionIndex = quizQuestions.length;
+let currentQuestionIndex = 0;
+let timeLeft = 50;
+let timerInterval;
+let score = 0;
+let correct;
 
 //GENERATE THE QUESTIONS AND ANSWERS
 function generateQQ(){
     gameOver.style.display ="none";
-    if (currentQuestionIndex == finalQuestionIndex){
-        return showScore;
+    if (currentQuestionIndex === finalQuestionIndex){
+        return showScore();
     }
     let currentQ = quizQuestions[currentQuestionIndex];
     gameQuestions.innerHTML = "<p>" + currentQ.question + "<p>";
@@ -96,27 +100,18 @@ function generateQQ(){
     btnC.innerHTML = currentQ.choiceC;
     btnD.innerHTML = currentQ.choiceD;
 
-}
-
-//need to show score 
-function showScore(){
-    quizBody.style.display = "none";
-    gameOver.style.display = "flex";
-    clearInterval(timerInterval);
-    inputName.ariaValueMax = "";
-    finalScores.innerHTML = "You scored " + score + " out of " + quizQuestions.length + " correct!";
-}
+};
 
 //START QUIZ FUNCTION
 function startQuiz(){
-    gameover.style.display = "none";
+    gameOver.style.display = "none";
     startQuizSection.style.display = "none";
     generateQQ();
 
     //Timer
     timerInterval = setInterval(function() {
         timeLeft--;
-        quizTimer.textContent = "Time left: " + timeLeft;
+        timer.textContent = "Time Remaining: " + timeLeft;
     
         if(timeLeft === 0) {
           clearInterval(timerInterval);
@@ -125,6 +120,17 @@ function startQuiz(){
       }, 1000);
     quizBody.style.display = "block";
 }
+
+//need to show score 
+function showScore(){
+    quizBody.style.display = "none";
+    gameOver.style.display = "flex";
+    clearInterval(timerInterval);
+    inputName.value = "";
+    finalScores.innerHTML = "You scored " + score + " out of " + quizQuestions.length + " correct!";
+}
+
+submitBtn.addEventListener("click",  highscore);
 
 function highscore(){
     if (inputName.value === "") {
@@ -139,13 +145,77 @@ function highscore(){
         };
 
         gameOver.style.display = "none";
-        
+        scoreSection.style.display ="flex";
+        scoreSectionDiv.style.display = "block";
+        endGameBtn.style.display = "flex";
+
+        savedScores.push(currentHighScore);
+        localStorage.setItem('savedScores', JSON.stringify(savedScores));
+        generateHighScores();
     }
 }
 
-submitBtn.addEventListener("click",  highscore());
+function showTheHighScore(){
+    startQuizSection.style.display = "none"
+    gameOver.style.display = "none";
+    scoreSectionDiv.style.display = "flex";
+    scoreSection.style.display = "block";
+    endGameBtn.style.display = "flex";
+
+    generateHighscores();
+}
+
+function generateHighscores(){
+    displayName.innerHTML = "";
+    scoreDisplay.innerHTML = "";
+    var highscores = JSON.parse(localStorage.getItem("savedScores")) || [];
+    for (i = 0; i < highscores.length; i++){
+        var newNameSpan = document.createElement("li");
+        var newScoreSpan = document.createElement("li");
+        newNameSpan.textContent = highscores[i].name;
+        newScoreSpan.textContent = highscores[i].score;
+        displayName.appendChild(newNameSpan);
+        scoreDisplay.appendChild(newScoreSpan);
+    }
+}
 
 
 
 
-startQuizBtn.addEventListener("click", startQuiz());
+
+function clearTheScores(){
+    window.localStorage.clear();
+    displayName.textContent = "";
+    scoreDisplay.textContent = "";
+    
+}
+
+function replayTheQuiz(){
+    scoreSectionDiv.style.display ='none';
+    gameOver.style.display = 'none';
+    startQuizSection.style.display = 'flex';
+    timeLeft = 50;
+    score = 0;
+    currentQuestionIndex = 0;
+    
+}
+
+function checkAnswer(answer){
+    correct = quizQuestions[currentQuestionIndex].correctAnswer;
+
+    if (answer === correct && currentQuestionIndex !== finalQuestionIndex){
+        score++;
+        alert("Correct! :D");
+        currentQuestionIndex++;
+        generateQQ();
+    } else if (answer !== correct && currentQuestionIndex !== finalQuestionIndex){
+        alert("Incorrect! D:");
+        currentQuestionIndex++;
+        generateQQ();
+    } else {
+        showScore();
+    }
+}
+
+
+startQuizBtn.addEventListener("click", startQuiz);
